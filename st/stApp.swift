45c425d2231b814d15c8cf406e52a3212b15ad63
0x00daem0n <9muhammadsaleh@gmail.com>
2025-01-31 19:44:50 -0500

    Initial Commit

---
+++ st/stApp.swift
@@ -0,0 +1,17 @@
+//
+//  stApp.swift
+//  st
+//
+//  Created by xdaem0n on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct stApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stTests/stTests.swift
@@ -0,0 +1,17 @@
+//
+//  stTests.swift
+//  stTests
+//
+//  Created by xdaem0n on 1/31/25.
+//
+
+import Testing
+@testable import st
+
+struct stTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
